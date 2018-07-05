a8244ca154f8e2449463b19136bd901ba45a2323
Ashish Patel <ashishpatel0720@gmail.com>
2018-07-06 00:20:15 +0530

    IMAGE_OVERLAY: added in bigquery/architecture in bigquery.vue

--- src/components/projects/bigquery.vue
+++ src/components/projects/bigquery.vue
@@ -1,9 +1,12 @@
 <template>
     <!--<div class="columns">-->
     <!--<div class="column">-->
+
     <div class="inner-item">
+        <gallery :images="images" :index="index" @close="index = null"></gallery>
+
         <p class="ap-heading"><span style="font-size:2em">CarWale BigQuery</span></p>
-        <p align="center"><img src="https://imgd.aeplcdn.com/0x0/bigquery/bigquery_architecture.png"></p>
+        <p align="center"><img :src="images[0]" @click="index = 0"></p>
 
         <h4 id="about">About</h4>
         <p  style="font-size:1em">
@@ -23,18 +26,18 @@
         <p style="font-size:1em ">After getting responce from Query Proxy, it save responce to file named after the <strong>job Id</strong> .</p>
         <p style="font-size:1em">When User request for result it will generate result using pagination and return result to ther user.</p>
         <p style="font-size:1em"><strong>Query Proxy</strong> is also responsible for cleaning response files after certain amount of time.</p>
-        <p align="center"><img style='margin:1em;' src="/static/images/ProxyQuery.jpg" alt="proxyQuery" /></p>
+        <p align="center"><img style='margin:1em;'  :src="images[1]" @click="index = 1"  alt="proxyQuery" /></p>
         <br>
         <h4 id="mist-server">Mist Server</h4>
         <p style="font-size:1em">
-        <p style="font-size:1em;" ><img align="left" src="/static/images/mist.png" alt="proxyQuery" width="200" height="100" style="margin:1em;" /></p>
+        <p style="font-size:1em;" ><img align="left" :src="images[2]" @click="index = 2" alt="proxyQuery" width="200" height="100" style="margin:1em;" /></p>
         <p style="font-size:1em">MistServer is a full-featured, next-generation streaming media toolkit for OTT (internet streaming). It takes care of all the annoying little problems you come across in media streaming projects, allowing you to focus on what makes your product or service unique. The MistServer software and our accompanying services allow anyone to quickly gain and keep a competitive edge.</p>
         <p style="font-size:1em">MistServer is written entirely in C++ and comes with an easy to use and integrate JSON API.</p>
         <p style="font-size:1em">for knowing more about what is media server checkout mistserver <a href="https://mistserver.org/mistserver">documentation</a></p>
         <br>
         <h4 id="spark">Spark</h4>
         <p>
-        <p ><img  align="left" src="/static/images/spark.png"  style="margin:1em;" alt="Spark" width="200" height="100" /></p>
+        <p ><img  align="left" :src="images[3]" @click="index = 3" style="margin:1em;" alt="Spark" width="200" height="100" /></p>
         <p>Apache Spark™ is a fast and general engine for large-scale data processing</p>
         <p>Spark Run programs up to 100x faster than Hadoop MapReduce in memory, or 10x faster on disk.</p>
         <p>Spark offers over 80 high-level operators that make it easy to build parallel apps. And you can use it interactively from the Scala, Python and R shells.</p>
@@ -47,7 +50,23 @@
     </div>
 </template>
 <script>
+    import VueGallery from 'vue-gallery';
     export default {
+        data: function () {
+            return {
+                images: [
+                    'https://imgd.aeplcdn.com/0x0/bigquery/bigquery_architecture.png',
+                    '/static/images/ProxyQuery.jpg',
+                    '/static/images/mist.png',
+                    '/static/images/spark.png',
+                ],
+                index: null
+            };
+        },
+
+        components: {
+            'gallery': VueGallery
+        },
 
     };
 
